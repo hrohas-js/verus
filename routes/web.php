8d7f408a165870a3c7ef5f0d73e73ec8b21cfdb4
hrohas <hrohas@gmail.com>
2025-09-23 20:41:21 +0300

    deploy fix

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Verus Warehouse API',
+        'status' => 'running',
+        'version' => '1.0.0',
+        'timestamp' => now()->toISOString()
+    ]);
 });
